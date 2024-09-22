9ea4d42e978e05182954e08d3e352207efc65665
Diego Cedeño <127360179+Diego-Alreaver@users.noreply.github.com>
2024-09-21 21:48:26 -0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	m := http.NewServeMux()
+	m.HandleFunc("/", handlePage)
+
+	// Obtener el puerto desde la variable de entorno, con un valor predeterminado de 8080
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := ":" + port
+
+	srv := http.Server{
+		Handler:      m,
+		Addr:         addr,
+		WriteTimeout: 30 * time.Second,
+		ReadTimeout:  30 * time.Second,
+	}
+
+	// Iniciar el servidor
+	fmt.Println("server started on", addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func handlePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(200)
+	const page = `<html>
+<head></head>
+<body>
+	<p> Hi Docker, I pushed a new version! </p>
+</body>
+</html>
+`
+	w.Write([]byte(page))
+}
